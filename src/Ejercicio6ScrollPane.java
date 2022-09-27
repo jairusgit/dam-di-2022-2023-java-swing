ddfe4f51aab4470b093e230b40c7e4b7aa081167
Jairo <jairo.garcia@ceu.es>
2022-09-27 17:51:58 +0200

    Ejercicio 6

---
+++ src/Ejercicio6ScrollPane.java
@@ -0,0 +1,64 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Ejercicio6ScrollPane extends JFrame {
+
+    JTextArea textArea;
+    JScrollPane scrollPane;
+    public Ejercicio6ScrollPane(){
+
+        //Layout
+        initLayout();
+
+        //JTextArea
+        textArea = new JTextArea(getTexto());
+        textArea.setBounds(50,50,700,2000);
+        textArea.setLineWrap(true);
+        textArea.setEditable(false);
+
+        //JScrollPane
+        scrollPane = new JScrollPane(textArea);
+        scrollPane.setBounds(50,50,700,450);
+        add(scrollPane);
+
+        setVisible(true);
+    }
+
+    private String getTexto(){
+
+        String texto = "";
+        for (int i = 1; i <= 10; i++){
+            for (int j = 1; j <= 10; j++){
+                texto += i + "x" + j + " = " + (i*j) + "\n";
+            }
+            texto += "\n";
+        }
+        return texto;
+    }
+
+    private void initLayout(){
+
+        //Layout absoluto
+        setLayout(null);
+
+        //Tamaño de la ventana
+        setBounds(10,10,800,600);
+
+        //Título
+        setTitle("Ejercicio 6");
+
+        //No redimensionable
+        setResizable(false);
+
+        //Cerrar proceso al cerrar la ventana
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+
+    public static void main(String[] args) {
+        new Ejercicio6ScrollPane();
+    }
+
+}
