196299af75500f6ac95157126c2cfc52e1e02b10
Jairo <jairo.garcia@ceu.es>
2022-09-27 16:26:48 +0200

    Ejemplo 7a con resultado

--- src/Ejemplo7a.java
+++ src/Ejemplo7a.java
@@ -2,13 +2,25 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Ejemplo7a extends JFrame {
+public class Ejemplo7a extends JFrame implements ActionListener{
 
-    private JLabel labelTextField, labelTextArea;
+    private JLabel labelTextField, labelTextArea, labelEmail;
     private JTextField textField;
-    private JTextArea textArea;
+    private JTextArea textArea, textAreaCuerpo;
+
+    private JButton botonGenerar, botonBorrar;
 
     public Ejemplo7a(){
+
+        initLayout();
+        initForm();
+        initResultado();
+
+        //Muestro el JFrame
+        setVisible(true);
+    }
+
+    private void initLayout(){
 
         //Layout absoluto
         setLayout(null);
@@ -24,6 +36,10 @@ public class Ejemplo7a extends JFrame {
 
         //Cerrar proceso al cerrar la ventana
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+
+    private void initForm(){
 
         //Etiqueta
         labelTextField = new JLabel("Email:");
@@ -46,11 +62,65 @@ public class Ejemplo7a extends JFrame {
         textArea.setLineWrap(true); //Cambia de línea al llegar al final
         add(textArea);
 
-        //Muestro el JFrame
-        setVisible(true);
+        //Botón
+        botonGenerar = new JButton("Generar");
+        botonGenerar.setBounds(50,370,300,30);
+        botonGenerar.addActionListener(this);
+        add(botonGenerar);
+
+    }
+
+    private void initResultado(){
+
+        //Etiqueta
+        labelEmail = new JLabel();
+        labelEmail.setBounds(400,80,300,30);
+        add(labelEmail);
+
+        //JTextArea
+        textAreaCuerpo = new JTextArea();
+        textAreaCuerpo.setBounds(400,140,300,200);
+        textAreaCuerpo.setBackground(this.getBackground());
+        textAreaCuerpo.setEditable(false); //Impide que se pueda editar
+        textAreaCuerpo.setLineWrap(true); //Cambia de línea al llegar al final
+        add(textAreaCuerpo);
+
+        //Botón
+        botonBorrar = new JButton("Borrar");
+        botonBorrar.setBounds(400,370,300,30);
+        botonBorrar.addActionListener(this);
+        add(botonBorrar);
+
+        //Oculto por defecto
+        visualizacionResultado(false);
+    }
+
+    private void visualizacionResultado(boolean mostrar){
+
+        labelEmail.setVisible(mostrar);
+        textAreaCuerpo.setVisible(mostrar);
+        botonBorrar.setVisible(mostrar);
     }
 
     public static void main(String[] args) {
         new Ejemplo7a();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //Generar
+        if (e.getSource() == botonGenerar){
+            labelEmail.setText(textField.getText());
+            textAreaCuerpo.setText(textArea.getText());
+            visualizacionResultado(true);
+        }
+        //Borrar
+        else if (e.getSource() == botonBorrar){
+            textField.setText("");
+            textArea.setText("");
+            labelEmail.setText("");
+            textAreaCuerpo.setText("");
+            visualizacionResultado(false);
+        }
     }
 }
